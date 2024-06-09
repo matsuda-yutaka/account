2694922526349260251b47b871271a4c01bd932e
matsuda-yutaka <matsudayutaka@matsudayutakas-MacBook-Air.local>
2024-06-09 21:38:56 +0900

    19th commit

--- src/com/diworksdev/account/dao/UserCreateCompleteDAO.java
+++ src/com/diworksdev/account/dao/UserCreateCompleteDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import com.diworksdev.account.util.DBConnector;
 import com.diworksdev.account.util.DateUtil;
 
@@ -15,6 +17,8 @@ public class UserCreateCompleteDAO {
 	private String sql = "INSERT INTO account_info(family_name, last_name, family_name_kana, last_name_kana, mail, password, gender, postal_code, prefecture, address_1, address_2, authority, registered_time) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 	public void cerateUser(String family_name, String last_name, String family_name_kana, String last_name_kana, String mail, String password, String gender, String postal_code, String prefecture, String address_1, String address_2, String authority) throws SQLException {
+		BCryptPasswordEncoder bcpe = new BCryptPasswordEncoder();
+		String encodeedPassword = bcpe.encode(password);
 
 		try {
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
@@ -23,8 +27,10 @@ public class UserCreateCompleteDAO {
 			preparedStatement.setString(3, family_name_kana);
 			preparedStatement.setString(4, last_name_kana);
 			preparedStatement.setString(5, mail);
-			preparedStatement.setString(6, password);
+			preparedStatement.setString(6, encodeedPassword);
 			preparedStatement.setString(7, gender);
+			System.out.println(password);
+			System.out.println(encodeedPassword);
 			preparedStatement.setString(8, postal_code);
 			preparedStatement.setString(9, prefecture);
 			preparedStatement.setString(10, address_1);
