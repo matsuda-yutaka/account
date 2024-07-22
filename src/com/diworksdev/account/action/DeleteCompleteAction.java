a1c4e3111f19ca118a9af3e82fb754b8ce0c94df
matsuda-yutaka <matsudayutaka@matsudayutakas-MacBook-Air.local>
2024-07-22 20:47:53 +0900

    Ver.3 8th idがActionに入った

--- src/com/diworksdev/account/action/DeleteCompleteAction.java
+++ src/com/diworksdev/account/action/DeleteCompleteAction.java
@@ -34,6 +34,14 @@ public class DeleteCompleteAction extends ActionSupport implements SessionAware 
 		return result;
 	}
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getDelete_flag() {
 		return delete_flag;
 	}
