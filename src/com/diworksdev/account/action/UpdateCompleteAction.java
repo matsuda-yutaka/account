98c6c57c1dce6032db7f274eee6b1c57cd3104bd
matsuda-yutaka <matsudayutaka@matsudayutakas-MacBook-Air.local>
2024-07-29 22:04:52 +0900

    Ver.3 17th family_nameに値が入ってない

--- src/com/diworksdev/account/action/UpdateCompleteAction.java
+++ src/com/diworksdev/account/action/UpdateCompleteAction.java
@@ -42,17 +42,27 @@ public class UpdateCompleteAction extends ActionSupport implements SessionAware 
 			if(id.equals(updateCompleteDTOList.get(0).getId())){
 
 			System.out.println(id);
-			int rs = dao.Update(id, family_name, last_name, family_name_kana, last_name_kana, mail, password, gender, postal_code, prefecture, address_1, address_2, authority);
-
+			System.out.println(family_name);
+			updateCompleteDAO.Update(session.get("id").toString(),
+					session.get("family_name").toString(),
+					session.get("last_name").toString(),
+					session.get("family_name_kana").toString(),
+					session.get("last_name_kana").toString(),
+					session.get("mail").toString(),
+					session.get("password").toString(),
+					session.get("gender").toString(),
+					session.get("postal_code").toString(),
+					session.get("prefecture").toString(),
+					session.get("address_1").toString(),
+					session.get("address_2").toString(),
+					session.get("authority").toString());
 			result = SUCCESS;
 
 			}
-
 		} catch (Exception e) {
 			e.printStackTrace();
 			result = ERROR;
 		}
-
 		return result;
 	}
 
@@ -62,6 +72,102 @@ public class UpdateCompleteAction extends ActionSupport implements SessionAware 
 
 	public void setId(String id) {
 		this.id = id;
+	}
+
+	public String getFamily_name() {
+		return family_name;
+	}
+
+	public void setFamily_name(String family_name) {
+		this.family_name = family_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getFamily_name_kana() {
+		return family_name_kana;
+	}
+
+	public void setFamily_name_kana(String family_name_kana) {
+		this.family_name_kana = family_name_kana;
+	}
+
+	public String getLast_name_kana() {
+		return last_name_kana;
+	}
+
+	public void setLast_name_kana(String last_name_kana) {
+		this.last_name_kana = last_name_kana;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getPostal_code() {
+		return postal_code;
+	}
+
+	public void setPostal_code(String postal_code) {
+		this.postal_code = postal_code;
+	}
+
+	public String getPrefecture() {
+		return prefecture;
+	}
+
+	public void setPrefecture(String prefecture) {
+		this.prefecture = prefecture;
+	}
+
+	public String getAddress_1() {
+		return address_1;
+	}
+
+	public void setAddress_1(String address_1) {
+		this.address_1 = address_1;
+	}
+
+	public String getAddress_2() {
+		return address_2;
+	}
+
+	public void setAddress_2(String address_2) {
+		this.address_2 = address_2;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
 	}
 
 	@Override
