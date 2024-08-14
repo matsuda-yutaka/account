7145b40a11c6ecafaa61da665e7adc3a32d27d57
matsuda-yutaka <matsudayutaka@matsudayutakas-MacBook-Air.local>
2024-08-14 20:25:41 +0900

    Ver.4 2nd ハッシュ化PWだとログインできない

--- src/com/diworksdev/account/action/LoginAction.java
+++ src/com/diworksdev/account/action/LoginAction.java
@@ -23,6 +23,9 @@ public class LoginAction extends ActionSupport implements SessionAware{
 
 		loginDTOList=dao.select(mail, password);
 
+		System.out.println(this.password);
+		System.out.println(loginDTOList.get(0).getPassword());
+
 		if(this.mail.equals(loginDTOList.get(0).getMail()) && this.password.equals(loginDTOList.get(0).getPassword())){
 			session.put("loginDTOList", loginDTOList);
 
--- src/com/diworksdev/account/dao/LoginDAO.java
+++ src/com/diworksdev/account/dao/LoginDAO.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import com.diworksdev.account.dto.LoginDTO;
 import com.diworksdev.account.util.DBConnector;
 
@@ -22,6 +24,7 @@ public class LoginDAO {
 		Connection con = db.getConnection();
 
 		String sql = "select * from account_info where mail=? and password=?";
+
 		try {
 			PreparedStatement ps = con.prepareStatement(sql);
 			ps.setString(1, mail);
@@ -29,14 +32,17 @@ public class LoginDAO {
 			ResultSet rs = ps.executeQuery();
 
 		while (rs.next()) {
-			LoginDTO dto=new LoginDTO();
+			//取得した結果を1件ずつDTOに格納し、更にDTOをArrayListに格納している
+			LoginDTO dto = new LoginDTO();
 			dto.setMail(rs.getString("mail"));
+			BCryptPasswordEncoder bcpe = new BCryptPasswordEncoder();
+			bcpe.matches(password, loginDTOList.get(0).getPassword());
 			dto.setPassword(rs.getString("password"));
 			loginDTOList.add(dto);
 		}
 
 		if(loginDTOList.size()<=0){
-			LoginDTO dto=new LoginDTO();
+			LoginDTO dto = new LoginDTO();
 			dto.setMail("該当なし");
 			dto.setPassword("該当なし");
 			loginDTOList.add(dto);
